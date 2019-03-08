6b68fadf33bc9e170ee234cbbeac75e22d85bc4f
Aadhar-windows-10 <aadhar41@gmail.com>
2019-03-08 15:55:02 +0530

    Laravel ORM intigration

--- app/controllers/home.php
+++ app/controllers/home.php
@@ -2,6 +2,12 @@
 
 class Home extends Controller
 {
+    protected $user;
+
+    public function __construct() 
+    {
+        $this->user = $this->model('User');
+    }
 
     public function index($name = '')
     {
@@ -9,6 +15,21 @@ class Home extends Controller
         $user->name = $name;
 
         $this->view('home/index', ['name' => $user->name]);
+
+    }
+
+    public function create($username = '',$email = '')
+    {
+        // $this->user->create([
+        //     'username' => $username,
+        //     'email' => $email
+        // ]);
+
+        User::create([
+            'username' => $username,
+            'email' => $email,
+        ]);
+
     }
 
 
--- app/models/User.php
+++ app/models/User.php
@@ -1,8 +1,15 @@
 <?php
 
-class User
+use Illuminate\Database\Eloquent\Model as Eloquent;
+
+class User extends Eloquent
 {
     public $name;
     
+    public $timestamps = ['created_at', 'updated_at'];
+
+    protected $fillable = ['username', 'email'];
+
+
 
 }
